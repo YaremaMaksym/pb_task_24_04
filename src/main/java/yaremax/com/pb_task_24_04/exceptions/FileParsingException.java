7a320bc64c9d97d6e6b9c24fa12c866624281cd5
Maksym Yarema <macsym2@gmail.com>
2024-04-30 14:49:12 +0300

    feat: add Exception handling

---
+++ src/main/java/yaremax/com/pb_task_24_04/exceptions/FileParsingException.java
@@ -0,0 +1,7 @@
+package yaremax.com.pb_task_24_04.exceptions;
+
+public class FileParsingException extends RuntimeException {
+    public FileParsingException(String message) {
+        super(message);
+    }
+}
