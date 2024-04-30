e6afd4cff6fd50efaf62a88d09462ef9cd5502ac
Maksym Yarema <macsym2@gmail.com>
2024-04-30 22:34:18 +0300

    feat: add AnimalSpecification

--- src/main/java/yaremax/com/pb_task_24_04/animal/AnimalService.java
+++ src/main/java/yaremax/com/pb_task_24_04/animal/AnimalService.java
@@ -1,7 +1,11 @@
 package yaremax.com.pb_task_24_04.animal;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +14,23 @@ public class AnimalService {
 
     public Animal addAnimal(Animal animal) {
         return animalRepository.save(animal);
+    }
+
+    public List<Animal> getAnimals(String type, Category category, String sex, Sort sort) {
+        Specification<Animal> spec = Specification.where(null);
+
+        if (type != null) {
+            spec = spec.and(AnimalSpecification.hasType(type));
+        }
+
+        if (category != null) {
+            spec = spec.and(AnimalSpecification.hasCategory(category));
+        }
+
+        if (sex != null) {
+            spec = spec.and(AnimalSpecification.hasSex(sex));
+        }
+
+        return animalRepository.findAll(spec, sort);
     }
 }
